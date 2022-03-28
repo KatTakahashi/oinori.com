111a751d3571da046883d87ad56a09e88d4f0918
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-03-28 03:48:41 +0000

    [Add]homes_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :destroy] do
+  root 'homes#top'
+  resources :posts, only: [:create, :destroy] do
     resource :post_lol, only:[:create]
   end
 end
