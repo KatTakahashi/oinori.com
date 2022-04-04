a1432c827463eae562e6191aeacec2885c204c10
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-04 08:51:41 +0000

    [Update]lol authority for poster

--- app/controllers/lols_controller.rb
+++ app/controllers/lols_controller.rb
@@ -2,7 +2,9 @@ class LolsController < ApplicationController
     # ---------- lol(いいね)機能 ----------
   def create
     @post = Post.find(params[:post_id])
-    lol = Lol.new(post_id: @post.id)
-    lol.save
+    unless post.ip == request.ip
+      lol = Lol.new(post_id: @post.id)
+      lol.save
+    end
   end
 end
