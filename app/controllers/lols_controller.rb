cdb89106d9ca9067f31d49def8cb95996f5939e0
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-04 11:03:55 +0000

    [Update]condition syntax error

--- app/controllers/lols_controller.rb
+++ app/controllers/lols_controller.rb
@@ -2,7 +2,7 @@ class LolsController < ApplicationController
     # ---------- lol(いいね)機能 ----------
   def create
     @post = Post.find(params[:post_id])
-    unless post.ip == request.ip
+    unless @post.ip == request.ip
       lol = Lol.new(post_id: @post.id)
       lol.save
     end
