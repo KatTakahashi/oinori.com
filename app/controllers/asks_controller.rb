5f0b088be742f0296e44a37b3f33c6192d59b981
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-09 08:16:55 +0000

    [Add]asks controller

---
+++ app/controllers/asks_controller.rb
@@ -0,0 +1,19 @@
+class AsksController < ApplicationController
+  # ---------- 逆質問一覧ページ ----------
+  def index
+  end
+
+  # ---------- 投稿機能 ----------
+  def create
+  end
+
+  # ---------- 削除機能 ----------
+  def destroy
+  end
+
+  private
+  # --------------- ストロングパラメータ --------------
+  def ask_params
+  end
+
+end
