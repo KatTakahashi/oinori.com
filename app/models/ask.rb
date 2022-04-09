7ee5cfe9feae37124ac0965bee1d26258d6bb84e
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-09 08:24:53 +0000

    [Add]model settings

--- app/models/ask.rb
+++ app/models/ask.rb
@@ -1,2 +1,16 @@
 class Ask < ApplicationRecord
+  # --------------- コールバック ---------------
+  before_save :trim
+
+  # --------------- アソシエーション ---------------
+  has_many :lols, dependent: :destroy
+
+  # --------------- バリデーション ---------------
+  validates :body, presence: true
+
+  private
+    # --------------- メソッド：投稿文の改行を削除 ---------------
+    def trim
+      body.strip!
+    end
 end
