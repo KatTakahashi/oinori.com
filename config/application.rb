09236071164b5bed565a0c50fa27c5929233a44d
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-11 00:57:10 +0000

    [Update]timezone to Tokyo

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module OinoriCom
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # タイムゾーンを東京に設定
+    config.time_zone = 'Tokyo'
   end
 end
